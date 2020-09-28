14f34bda31fcc5826fdcd48a32b11cef62840ca5
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-28 11:54:27 +0900

    Create Game2IncDecTest.java

---
+++ Game2IncDecTest.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    int x, y, z;
+
+    x = 10; y = 5; z = 0;
+    z = x++ - y-- + 1;
+    System.out.println("x = " + x + ", y = " + y + ", z = " + z);
+
+    x = 10; y = 5; z = 0;
+    z = ++x - --y + 1;
+    System.out.println("x = " + x + ", y = " + y + ", z = " + z);
+  }
+}
