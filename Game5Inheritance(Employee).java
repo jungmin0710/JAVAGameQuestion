a988bab321c49fe7898f4fba94d58777f15392ef
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-29 15:18:14 +0900

    Create Game5Inheritance(Employee).java

---
+++ Game5Inheritance(Employee).java
@@ -0,0 +1,13 @@
+public class Employee{
+  String name;
+  int salary;
+
+  public Employee(String n, int s){
+    name = n;
+    salary = s;
+  }
+
+  public void getInformation(){
+    System.out.println("이름 : " + name + ", 연봉 : " + salary);
+  }
+}
