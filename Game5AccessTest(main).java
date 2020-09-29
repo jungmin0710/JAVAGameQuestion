49429a6218a50d7ede280d3a20897f77cbff2ce1
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-29 16:04:36 +0900

    Create Game5AccessTest(main).java

---
+++ Game5AccessTest(main).java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args){ 
+    D d = new D();
+    E e = new E();
+    F f = new F();
+
+    d.a = 500;
+    // e.b = 500;
+    // f.c = 500;
+
+    d.method_A();
+    // e.method_B();
+    // f.method_C();
+
+    d.method_D();
+    e.method_E();
+    f.method_F();
+
+    System.out.println("a = " + d.a);
+    // System.out.println("b = " + e.b);
+    // System.out.println("c = " + f.c);
+  }
+}
