7f48433527d1423fd78b1fa6c1c8f9bb97b2cb41
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-29 16:05:34 +0900

    Create Game5AccessTest(B).java

---
+++ Game5AccessTest(B).java
@@ -0,0 +1,15 @@
+public class B{
+  private int b;
+  
+  private void method_B(){
+    b = 200;
+  }
+}
+
+/*
+
+private 가장 폐쇄적인 접근제어자이다.
+private으로 선언된 변수, 메서드 등은 해당 클래스 내에서만 사용 가능하다. 다른 클래스로부터 보호하고 싶은 변수, 메서드 앞에 사용한다.
+실제 프로그래머들은 이후에 변경할 가능성이 있을 때 주로 private을 사용한다.
+
+*/
